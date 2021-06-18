e236fa9a85ca3daad6e848bf1c5d7512b3db34ee
ivsonsantos <Belgique@2024>
2021-06-18 09:43:38 +0200

    Navigate based on the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng4-complete-guide';
+  title = 'Ivson';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
 }
