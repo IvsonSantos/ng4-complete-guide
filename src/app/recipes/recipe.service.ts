dc6b412127df940c481d68db28b0babf756d1cd2
ivsonsantos <Belgique@2024>
2021-07-01 13:41:38 +0200

    List of Ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,16 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test recipe','Simple test','https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F05%2F08%2FChewy-Peanut-Butter-Brownies-by-abcdeelishus.jpg&q=85'),
-        new Recipe('Second','Simple test','https://portalcontexto.com/wp-content/uploads/2019/06/lasanha-bolonhesa.jpg')
+        new Recipe('A Test recipe','Simple test','https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F05%2F08%2FChewy-Peanut-Butter-Brownies-by-abcdeelishus.jpg&q=85',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20),
+        ]),
+        new Recipe('Second','Simple test','https://portalcontexto.com/wp-content/uploads/2019/06/lasanha-bolonhesa.jpg',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+        ])
       ];
 
     getRecipes() {
