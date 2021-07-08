90876c2e9856360cdf1817ac8067d0d1ee3823f1
ivsonsantos <Belgique@2024>
2021-07-08 08:31:11 +0200

    Passing parameters to links

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,8 +9,7 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe!: Recipe;
-
-  constructor() { }
+  @Input() index!: number;
 
   ngOnInit() {
   }
