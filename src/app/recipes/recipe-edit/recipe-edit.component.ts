7f7886d58a15ec4773712cd0c4157a0915b4aef5
ivsonsantos <Belgique@2024>
2021-07-08 09:15:52 +0200

    Edit mode

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  templateUrl: './recipe-edit.component.html'
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  editMode = false;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = params['id'] != null;
+        console.log(this.editMode);
+      }
+    )
   }
 
 }
